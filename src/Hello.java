66dbbe7ff0f12331ced45fffeb49d8af8e5618e7
wingsteven <wings253@strose.edu>
2019-01-22 15:24:46 -0500

    A successful edit to the hello.java file

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,15 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello "+System.getProperty("user.name"));
-
+		System.out.println("This is an edit for Question 9!");
+		int x = 27;
+		int y = 32;
+		System.out.println("These are two variables:\nx = "+x+"\ny = "+y);
+		x = x + y;
+		y = x - y;
+		x = x - y;
+		System.out.println("The variables' values have been swapped using math:\nx = "+x+"\ny = "+y);
+		
 	}
 
 }
